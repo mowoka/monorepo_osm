80b3a3420f12d9e481c4e8a645efc2355cbe1b35
Giofanny Mowoka <giofanny@softwareseni.com>
2024-12-24 04:58:37 +0700

    fix: linter issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,30 @@
-const path = require('path')
-
 module.exports = {
-    env: {
-        es2021: true,
+  extends: ['next', 'eslint:recommended', 'prettier'],
+  plugins: ['prettier'],
+  settings: {
+    next: {
+      rootDir: 'apps/next/',
     },
-    extends: ['standard-with-typescript', 'prettier'],
-    parserOptions: {
-        project: path.join(__dirname, '/tsconfig.json'),
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+  },
+  root: true,
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'off',
+    'no-console': 'warn',
+  },
+  overrides: [
+    {
+      files: ['./**/*.ts', './**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      extends: ['plugin:@typescript-eslint/recommended'],
+      plugins: ['@typescript-eslint'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'error',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/ban-types': 'off',
+      },
     },
-    overrides: [],
+  ],
 }
